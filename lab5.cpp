3819a921ff3b94aae7d9f0627e191174d7e9d15d
FabianVladmir <49292220+FabianVladmir@users.noreply.github.com>
2020-06-09 23:51:27 -0500

    Add files via upload

--- lab5.cpp
+++ lab5.cpp
@@ -8,13 +8,25 @@ private:
     friend class linkedList;
 public:
     Nodo();
+    Nodo(int element_);
+    Nodo(int p, Nodo* n);    
     ~Nodo();
 };
 
-Nodo::Nodo()
+Nodo::Nodo(){
+    this->element = 0;
+    this->next = nullptr;
+}
+
+Nodo::Nodo(int element_)
 {
-    this->element = element;
-    next->next = nullptr;
+    this->element = element_;
+    this->next = nullptr;
+}
+
+Nodo::Nodo(int p, Nodo *n){
+    this->element = p;
+    this->next = n;
 }
 
 Nodo::~Nodo()
@@ -23,65 +35,101 @@ Nodo::~Nodo()
 
 typedef Nodo* nodoptr;
 
-
 class linkedList
 {
 private:
-    int size;
+    int size;    
     Nodo *head;
-public:
+public:    
     linkedList();
-    void insertar(int valor, Nodo *p);
-    void remove(Nodo* v, int eliminar);
+    void insertar(int valor);
+    void remove(int eliminar);
     void print();
     ~linkedList();
 };
 
-linkedList::linkedList()
-{
-    this->size = size;
-    this->head = head;
-}
-void linkedList :: insertar(int valor, Nodo *p){
-    if (valor < 0 || p->element < valor)
-    {
-        p->next->element = valor;
-        valor = p->element;
-    }else
-    {
-        nodoptr aux;
-        aux->element = valor;
-        while (aux->next != nullptr && aux->next->element < p->element)
-        {
-            aux = aux->next;
-        }
-        valor = aux->next->element;
-        aux->next->element = valor;
-    }
+linkedList::linkedList( ){ 
+    this->size = 0;   
+    this->head = nullptr;
 }
 
-void linkedList :: remove(Nodo *v, int eliminar){
-    if (v != nullptr)
+void linkedList :: insertar(int valor){
+   
+    if (head == nullptr) // case 1
     {
-        nodoptr aux = v;
-        if (v->element == eliminar)
+        head = new Nodo;
+        head->element = valor;
+        head->next = nullptr;
+    }else
+    {
+        nodoptr p = head;
+        if (valor < p->element) // case 2
         {
-            v = v->next;
-            delete aux;
-        }else
+            p = new Nodo(valor);
+            p->next = head;
+            head = p;
+        }
+        while (p->next != nullptr)
         {
-            while (v->next != nullptr && v->next->element != eliminar)
+            if (valor > p->next->element) // case 3
             {
-                aux = aux->next;
-            }
-            if (v->next->element == eliminar)
+                p = p->next;
+            }else
             {
-                nodoptr aux2 = aux->next;
-                aux->next = aux2->next;
-                delete aux2;
+                nodoptr aux;
+                aux->element = valor;
+                aux->next = p->next;
+                p->next = aux;
+                return;
             }
         }
+        if (valor > p->element)
+        {
+            if (head != nullptr)
+            {
+                p = new Nodo(valor);
+                nodoptr it = head;
+                while (it->next != nullptr)
+                {
+                    it = it->next;
+                }
+                it->next = p; 
+            }else
+            {
+                p = new Nodo(valor);
+                p->next = head;
+                head = p;
+            } 
+        }
     }
+    size++;
+}
+
+void linkedList :: remove(int eliminar){
+    if (head == nullptr) // case 1
+    {
+        return;
+    }
+
+    if (head->element == eliminar) // case2
+    {
+        nodoptr aux = head;
+        head = head->next;
+        delete aux;
+        return;        
+    }
+    nodoptr aux = head;
+    while (aux->next != nullptr && aux->next->element != eliminar) // case3
+    {
+        aux = aux->next;
+        if (aux->next->element == eliminar)
+        {
+            nodoptr aux2 = aux->next;
+            aux->next = aux2->next;
+            delete aux2;
+        }  
+    }    
+    
 }
 
 void linkedList ::print(){
@@ -96,7 +144,7 @@ linkedList::~linkedList()
 {
     while (head != nullptr)
     {
-        nodoptr p = head;
+        nodoptr p = head; 
         head = head->next;
         cout << "Eliminando: " << p->element << endl;
         delete p;
@@ -105,8 +153,23 @@ linkedList::~linkedList()
 
 
 int main(){
+    linkedList lista;
+    for (int i = 0; i < 20; i+=2)
+    {
+        lista.insertar(i);
+    }
     
+    lista.print();
+    for (int i = 0; i < 10; i++)
+    {
+        if (i % 5 == 0)
+        {
+            lista.remove(i);
+        }
+        
+    }
     
+    lista.print();
 
     return 0;
 }
